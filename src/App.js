2131627a964e611dad63ed4f59c05b1fac4d819f
shaznan <shaznanfairozesally@gmail.com>
2021-04-06 19:52:29 +0530

    Creat shop page preview for each item catergory

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import "./App.css";
 import HomePage from "./Components/pages/homepage.component/homepage.components";
 import { Route, Switch } from "react-router-dom";
+import ShopPage from "./Components/shop/shop.components";
 
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>HATS PAGE</h1>
-    </div>
-  );
-};
+// const HatsPage = () => {
+//   return (
+//     <div>
+//       <h1>HATS PAGE</h1>
+//     </div>
+//   );
+// };
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/Components/previewCollection/collection-preview.component.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./collection-preview.style.scss";
+import CollectionItem from "../collection-item/collectionitem.components";
+
+const CollectionPreview = ({ title, items }) => (
+  <div className="collection-preview">
+    <h1 className="title">{title.toUpperCase()}</h1>
+    <div className="preview">
+      {items
+        .filter((item, i) => i < 4)
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
+        ))}
+    </div>
+  </div>
+);
+
+export default CollectionPreview;
